0610ae112a71400276f46e4bb9b1c09fa603f54c
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-14 00:36:54 +1000

    build 0.01

---
+++ docs/precache-manifest.53b4d9325b361a915a7823efef18adfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51d52fc803a8e62e291ba1ac698bc08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0454a514fb1f352b876",
+    "url": "/static/css/main.9e33d809.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "d0454a514fb1f352b876",
+    "url": "/static/js/main.3eb1807e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/static/media/digital-7.regular.1e670d88.ttf"
+  }
+]);
